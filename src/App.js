aed929344d16ce887fe2921f2f9ab9c76fd64b16
Gemma Fong <115029730+smolTypo@users.noreply.github.com>
2023-04-06 21:24:16 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Products from './Components/products'
+import "bootstrap/dist/css/bootstrap.css";
+import '../src/Components/styles.css';
+
+function App() {
+  return (
+  <Products></Products>
+  );
+}
+
+export default App;
